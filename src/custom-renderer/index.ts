ad921e4b375f360ab6929b59174e829747604a94
何南松 <nansen.ho@foxmail.com>
2023-04-18 19:58:58 +0800

    refactor

---
+++ src/custom-renderer/index.ts
@@ -0,0 +1,53 @@
+import { createRenderer } from "vue";
+import { Container, Text, Sprite, Texture } from "pixi.js";
+
+export const { createApp } = createRenderer<Container, Container>({
+  createElement(type) {
+    let element;
+    switch (type) {
+      case "Container":
+        element = new Container();
+        break;
+      case "Sprite":
+        element = new Sprite();
+        break;
+      default:
+        throw new Error(`This type (${type}) does not exist`);
+        break;
+    }
+    return element;
+  },
+  insert(el, parent) {
+    if (el && parent) {
+      parent.addChild(el);
+    }
+  },
+  remove(el) {
+    if (el && el.parent) {
+      el.parent.removeChild(el);
+    }
+  },
+  createText(text) {
+    return new Text(text);
+  },
+  patchProp(el, key, prevValue, nextValue) {
+    switch (key) {
+      case "texture":
+        (el as Sprite).texture = Texture.from(nextValue);
+        break;
+      default:
+        break;
+    }
+  },
+  createComment(text) {
+    return new Text(text);
+  },
+  setText() {},
+  setElementText() {},
+  parentNode(node) {
+    return node.parent;
+  },
+  nextSibling() {
+    return null;
+  },
+});
