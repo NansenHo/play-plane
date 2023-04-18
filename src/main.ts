bd4bfa1641a84dad38c9aa6337f9673ff91d5eb9
何南松 <nansen.ho@foxmail.com>
2023-04-18 17:02:31 +0800

    init pixijs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from "vue";
+// import "./style.css";
+// import App from "./App.vue";
 
-createApp(App).mount('#app')
+// createApp(App).mount("#app");
+import { Application } from "pixi.js";
+
+const game = new Application({
+  width: 500,
+  height: 500,
+});
+
+document.body.append(game.view);
